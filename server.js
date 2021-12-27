c52c67e20fd1a32ab0f144e48a4d477a863a285c
jack <jackmanzer88@gmail.com>
2021-12-27 10:17:35 -0600

    revised models folder

--- server.js
+++ server.js
@@ -23,11 +23,12 @@ mongoose.connection.on('connected', () => {
 });
 
 //import models 
-const Character = require("./models/character");
-const Planet = require("./models/planet");
-const Species = require("./models/species");
-const Weapon = require("./models/weapon");
-const Vehicle = require("./models/vehicle");
+const { Character, Planet, Species, Weapon, Vehicle} = require('./models');
+// const Character = require("./models/character");
+// const Planet = require("./models/planet");
+// const Species = require("./models/species");
+// const Weapon = require("./models/weapon");
+// const Vehicle = require("./models/vehicle");
 
 app.get("/", (req, res) => {
   res.sendFile('index.html', { root: 'public' })
